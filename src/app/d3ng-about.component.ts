5848e909dc874a7b2badb0186e3fca41dd2e30aa
Markus Scheidgen <markus.scheidgen@gmail.com>
2017-06-12 14:37:19 +0200

    Various improvement to the public presentation of D3ng. Rolled back to ES5 to support the angular2 build process. Fixed code to build in production mode. Added introduction to all examples and pages. Added an about page. Minor bugfixes.

---
+++ src/app/d3ng-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-d3ng-about',
+  templateUrl: './d3ng-about.component.html',
+  styleUrls: ['./d3ng-about.component.css']
+})
+export class D3ngAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
