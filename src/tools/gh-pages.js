a6ea7ded61b2f3730737f5b45e941e0090ff337f
Markus Scheidgen <markus.scheidgen@gmail.com>
2017-06-19 11:35:21 +0200

    Fixed gh-pages.

--- src/tools/gh-pages.js
+++ src/tools/gh-pages.js
@@ -1,9 +1,8 @@
 const ghpages = require('gh-pages');
 
-const options = {
-  branch: 'master',
-  repo: 'git@github.com:d3ng/d3ng.github.io.git'
-}
-ghpages.publish('dist/app', options, function(err) {
+ghpages.publish('dist/app', {
+  repo: 'https://github.com/d3ng/d3ng.github.io.git',
+  branch: 'master'
+}, function(err) {
   console.log(err.toString());
 });
